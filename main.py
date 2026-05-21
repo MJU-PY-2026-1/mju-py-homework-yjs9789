08ee9d8c3c3780c3cd6d08af84e01202ca980181
yjs9789 <yjs9789@mju.ac.kr>
2026-05-21 10:09:01 +0000

    60231808-양준서-과제3차완료

--- main.py
+++ main.py
@@ -1,3 +1,46 @@
+
+
+current_calories=0
+
+def record_diet():
+    global current_calories
+    target_calories=2500
+    calories=float(input('섭취한 칼로리:'))
+    current_calories+=calories
+
+    print(f'\n현재 총 섭취량:{current_calories} / {target_calories}kal')
+
+
+
+def cal_exercises_volume():
+    total_volume=0
+    sets=int(input('세트 수:'))
+    if sets<=0 :
+        return 0            
+    for _ in range(1, sets+1):
+        weight=float(input('무게:'))
+        num=int(input('횟수:'))  
+        if weight <= 0 or num<=0:
+            break
+        total_volume+= weight*num
+    return total_volume
+        
+
+def cal_bmi (bodyheight_cm, bodyweight):
+        bodyheight_m = bodyheight_cm /100
+        bmi=bodyweight/(bodyheight_m**2)
+        if bmi <=18.5:
+            my_bmi='저체중'
+        elif bmi>18.5 and bmi<=23:
+            my_bmi='정상체중'
+        elif bmi>23 and bmi<=25:
+            my_bmi='과체중'
+        else: 
+            my_bmi='비만'
+        
+        return bmi, my_bmi
+
+
 while True:
     print('\n -----메뉴----')
     print('1. 식단기록')
@@ -9,35 +52,11 @@ while True:
 
     menu=input('메뉴 선택(1~6):')
 
-    def cal_exercises_volume():
-        total_volume=0
-        sets=int(input('세트 수:'))
-        if sets<=0 :
-            return 0            
-        for n in range(1, sets+1):
-            weight=float(input('무게:'))
-            num=int(input('횟수:'))  
-            if weight <= 0 or num<=0:
-                break
-            total_volume+= weight*num
-            return total_volume
+    if menu=='1':
+        record_diet()
+
         
-    def cal_bmi (bodyheight_cm, bodyweight):
-            bodyheight_m = bodyheight_cm /100
-            bmi=bodyweight/(bodyheight_m**2)
-            if bmi <=18.5:
-                my_bmi='저체중'
-            elif bmi>18.5 and bmi<=23:
-                my_bmi='정상체중'
-            elif bmi>23 and bmi<=25:
-                my_bmi='과체중'
-            else: 
-                my_bmi='비만'
-            return bmi, my_bmi
-
-
-
-    if menu=='2':
+    elif menu=='2':
         mon_exercises=[1,2]
 
         exercises_volume=[] 
@@ -46,18 +65,19 @@ while True:
             volume=cal_exercises_volume()
             exercises_volume.append([exercise, volume])
         for n in exercises_volume:
-            print(f'{n[0]}, {n[1]}')
+            print(f'\n{n[0]}, {n[1]}')
 
-
+    
     elif menu=='3' :
         bodyheight_cm=float(input('키를 입력하시오:'))
         bodyweight=float(input('몸무게를 입력하시오:'))
         bmi, my_bmi= cal_bmi(bodyheight_cm, bodyweight)
-        print(f'bmi={bmi: .2f}, {my_bmi}입니다')
+        print(f'\nbmi={bmi: .2f}, {my_bmi}입니다')
 
+    
     elif menu=='6':
         print('종료')
         break
 
     else:
-        print('없는 메뉴')
+        print('1~6선택')
