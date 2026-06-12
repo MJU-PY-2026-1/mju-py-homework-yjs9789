abf88ed3ffa741a939c0190b976d279c7aa0de8e
yjs9789 <yjs9789@mju.ac.kr>
2026-06-12 07:53:16 +0000

    60231808-양준서-과제4차완료

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 
 
 current_calories=0
+exercises_volume=[] 
 
 def record_diet():
     global current_calories
     target_calories=2500
-    calories=float(input('\n섭취한 칼로리:'))
-    current_calories+=calories
-
-    print(f'현재 총 섭취량:{current_calories} / {target_calories}kal')
+    
+    try:
+        calories=float(input('\n섭취한 칼로리:'))
+        current_calories+=calories
+        print(f'현재 총 섭취량:{current_calories} / {target_calories}kal')
+    except ValueError:
+        print('칼로리를 숫자로 입력')
 
 
 
 def cal_exercises_volume():
     total_volume=0  
-    sets=int(input('세트 수'))
 
-    if sets<=0 :
-        return 0            
-    for i in range(1, sets+1):
-        weight_num=input(f'{i}세트의 무게와 횟수를 띄어쓰기로 입력 ').split()
+    try:
+        exercise, sets=input('\n운동 종목과 세트 수 띄어쓰기로 입력:').split()          
         
-        weight=float(weight_num[0])
-        num=int(weight_num[1])
-        if weight<=0 or num<=0:
-            break
-        total_volume+= weight*num
-    return total_volume
+        for i in range(1, sets+1):
+            weight,num=input(f'{i}세트의 무게와 횟수를 띄어쓰기로 입력 ').split()
+            
+            weight=float(weight)
+            num=int(num)
+            if weight<=0 or num<=0:
+                break
+            total_volume+= weight*num
+    except ValueError:
+        print('세트 수, 무게, 횟수를 숫자로 입력')
+    return exercise, sets, total_volume
         
 
 def cal_bmi (bodyheight_cm, bodyweight):
@@ -50,7 +56,7 @@ while True:
     print('2. 운동기록')
     print('3. 신체 정보 업데이트')
     print('4. 오늘의 루틴 체크')
-    print('5. 운동루틴 확인 ')
+    print('5. 운동기록 확인, 저장 ')
     print('6. 종료')
 
     menu=input('메뉴 선택(1~6):')
@@ -62,23 +68,34 @@ while True:
     elif menu=='2':
         mon_exercises=[1,2]
 
-        exercises_volume=[] 
         for i in range(len(mon_exercises)): 
-            exercise=input('\n운동 종목:')      
-            volume=cal_exercises_volume()
-            exercises_volume.append([exercise, volume])
-        
-        print()
-        for n in exercises_volume:
-            print(f'{n[0]}, {n[1]}')
+                  
+            exercise, sets,volume=cal_exercises_volume()
+            exercises_volume.append([exercise, sets, volume])
 
     
     elif menu=='3' :
-        bodyheight_cm=float(input('키를 입력하시오:'))
-        bodyweight=float(input('몸무게를 입력하시오:'))
-        bmi, my_bmi= cal_bmi(bodyheight_cm, bodyweight)
+        try:
+            bodyheight_cm=float(input('키를 입력하시오:'))
+            bodyweight=float(input('몸무게를 입력하시오:'))
+            bmi, my_bmi= cal_bmi(bodyheight_cm, bodyweight)
+        except ValueError:
+            print('키와 몸무게를 숫자로 입력')
         print(f'\nbmi={bmi: .2f}, {my_bmi}입니다')
 
+
+    elif menu=='5':
+        
+        print('\n---------------------------------------------------------------------')
+        for row in exercises_volume:
+            for column in row:
+                print(f'\n{column[0]}   |   {column[1]}   |   {column[2]}   ')
+        print('\n---------------------------------------------------------------------')
+
+        with open('exercises_record', 'w', encoding='ust-8')as file:
+            for row in exercises_volume:
+                for column in row:
+                    file.write(f'\n{column[0]}   |   {column[1]}   |   {column[2]}   ')
     
     elif menu=='6':
         print('종료')
